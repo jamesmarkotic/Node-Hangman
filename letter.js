9517986cdced00593777bb51273d80f4f0dc01a5
jamesmarkotic <jamesmarkotic@gmail.com>
2018-02-03 23:00:01 -0800

    minor additions

---
+++ letter.js
@@ -0,0 +1,32 @@
+
+
+// Letter constructor
+function Letter (letter) {
+
+  this.letter = letter;
+  this.guessed = false;
+  this.uScore = '_';
+  this.check = function () {
+    console.log("I'm running");
+    if (this.guessed == true) {
+      this.uScore = this.letter;
+      // console.log(this.guessed);
+    }
+
+  };
+  this.correct = function (character) {
+    if (character == this.letter) {
+      this.guessed = true;
+    }
+  };
+
+}
+// End of Letter constructor
+
+// Testing     _ _ _ _ z
+// var test = new Letter ('z', false);
+// 
+// console.log(test.guessed);
+// console.log(test.uScore);
+
+module.exports = Letter;
